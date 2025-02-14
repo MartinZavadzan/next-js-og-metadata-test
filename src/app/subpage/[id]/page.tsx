0c1b0aba7bdd36b1ae242c0842942a30af7c7beb
Martin Zavadzan <martin.zavadzan@vacuumlabs.com>
2025-02-14 16:16:51 +0100

    init

---
+++ src/app/subpage/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { getImageAddress } from "@/data"
+
+type Props = {
+    params: Promise<{ id: string }>
+  }
+  
+  export async function generateMetadata({ params }: Props) {
+    const id = (await params).id as string
+    return {
+      title: `Page with id ${id}`,
+      description: `Some description for page with ID ${id}`,
+      image: await getImageAddress(),
+    }
+  }
+
+export default function Page() {
+    return <div>Subpage</div>
+}
---
+++ src/data.ts
@@ -0,0 +1,1 @@
+export const getImageAddress = async () => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9MagkuGc5nFUrjBzZ-V8R93ntAPXpQd_2_A&s";
