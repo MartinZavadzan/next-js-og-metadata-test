648333a53647b5d34f603375dda5640e36b546e9
Martin Zavadzan <martin.zavadzan@vacuumlabs.com>
2025-02-14 16:47:59 +0100

    add smaller card

---
+++ src/app/twitterSmallCard/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { getImageAddress } from "@/data";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props) {
+  const id = (await params).id as string;
+  return {
+    twitter: {
+      title: `Page with id ${id}`,
+      description: `Some description for page with ID ${id}`,
+      card: "summary",
+      images: [await getImageAddress()],
+    },
+  };
+}
+
+export default function Page() {
+  return <div>Subpage</div>;
+}
---
+++ src/app/twitterSmallCard/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+export default function Page() {
+    const router = useRouter()
+    useEffect(()=>{
+        router.replace(`/twitterSmallCard/1`)
+    },[router])
+
+    return <div>Page</div>
+}
